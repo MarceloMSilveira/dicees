130f7e9fce4d9d804fff31da9cc00f7152767b8f
Marcelo Silveira <marcelomendes.siveira@gmail.com>
2024-04-09 14:18:32 -0300

    play dice one

--- index.js
+++ index.js
@@ -2,5 +2,19 @@ function playDicee() {
     return Math.floor(6*Math.random()+1);
 }
 
+function getImageName(imgNr){
+    var imageName = "dice";
+    imageName = imageName + imgNr + ".png";
+    return imageName;
+}
+
 var randonNumber1 = playDicee();
 var randonNumber2 = playDicee();
+
+for (var i=1; i<7; i++){
+    if (i===randonNumber1){
+        var imageName = getImageName(i);
+        imageName = "./images/"+imageName;
+        document.querySelector(".img1").setAttribute("src",imageName);
+    }
+}
