b31596b2eb0e0e043e8f2a0ed1349dd47b96ed6b
Marcelo Silveira <marcelomendes.siveira@gmail.com>
2024-04-09 14:45:17 -0300

    show winner

--- index.js
+++ index.js
@@ -8,6 +8,16 @@ function getImageName(imgNr){
     return imageName;
 }
 
+function checkWinner(Nr1,Nr2){
+    if (Nr1>Nr2) {
+        return 1;
+    } else if (Nr1<Nr2) {
+        return 2;
+    } else {
+        return 0; //draw
+    }
+}
+
 var randonNumber1 = playDicee();
 var randonNumber2 = playDicee();
 
@@ -23,4 +33,15 @@ for (var i=1; i<7; i++){
         imageName = "./images/"+imageName;
         document.querySelector(".img2").setAttribute("src",imageName);
     }
+}
+
+//check winner player or if is a draw
+var winner = checkWinner(randonNumber1,randonNumber2);
+
+if (winner===0) {
+    document.querySelector("h1").innerText = "Draw."
+} else if (winner===1) {
+    document.querySelector("h1").innerText = "Player 1 Wins!"
+} else {
+    document.querySelector("h1").innerText = "Player 2 Wins!"
 }
